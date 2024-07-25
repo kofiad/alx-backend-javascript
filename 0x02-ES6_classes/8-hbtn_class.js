0ff99bf7abb3d7f5d4d447ee8a76f171cae8fe51
kofiad <kofiadaba@gmail.com>
2024-07-25 23:12:19 +0000

    Refactor HolbertonClass to use ES6 syntax and conventions

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,25 +1,30 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
-    // size getter and setter
-    get size() {
-        return this._size;
-    }
-    set size(size) {
-        this._size = size;
-    }
-    // location getter and setter
-    get location() {
-        return this._location;
-    }
-    set location(location) {
-        this._location = location;
-    }
-    [Symbol.toPrimitive](hint) {
-        if (hint === 'string') return this.location;
-        if (hint === 'number') return this.size;
-        return this;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // size getter and setter
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  // location getter and setter
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') return this.location;
+    if (hint === 'number') return this.size;
+    return this;
+  }
 }
