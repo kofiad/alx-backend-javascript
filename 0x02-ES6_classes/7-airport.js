140a195fd9280ce765acccc2c291688d3fd902ca
kofiad <kofiadaba@gmail.com>
2024-07-25 22:22:04 +0000

    Refactor Airport class to use ES6 syntax and conventions

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,23 +1,28 @@
 export default class Airport {
-    constructor(name,code) {
-        this._name = name;
-        this._code = code;
-    }
-    //name getter and setter
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        this._name = name;
-    }
-    //code getter and setter
-    get code() {
-        return this._code;
-    }
-    set code(code) {
-        this._code = code;
-    }
-    get[Symbol.toStringTag]() {
-        return this._code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // name getter and setter
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  // code getter and setter
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }
