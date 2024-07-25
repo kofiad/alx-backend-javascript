0dc2e8918f29088827ceadd741350feec81d3632
kofiad <kofiadaba@gmail.com>
2024-07-25 21:33:33 +0000

    refactor: Update Car class to use ES6 syntax and conventions

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,39 +1,41 @@
 export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
+    constructor(brand, motor, color) {
+      this.brand = brand;
+      this.motor = motor;
+      this.color = color;
+    }
 
-  // brand getter and setter
-  get brand() {
-    return this._brand;
-  }
+    get brand() {
+      return this._brand;
+    }
 
-  set brand(brand) {
-    this._brand = brand;
-  }
+    set brand(value) {
+      this._brand = value;
+    }
 
-  // motor getter and setter
-  get motor() {
-    return this._motor;
-  }
+    get motor() {
+      return this._motor;
+    }
 
-  set motor(motor) {
-    this._motor = motor;
-  }
+    set motor(value) {
+      this._motor = value;
+    }
 
-  // color getter and setter
-  get color() {
-    return this._color;
-  }
+    get color() {
+      return this._color;
+    }
 
-  set color(color) {
-    this._color = color;
-  }
+    set color(value) {
+      this._color = value;
+    }
 
-  cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species();
+    static get [Symbol.species]() {
+      return this;
+    }
+
+    cloneCar() {
+      const Species = this.constructor[Symbol.species];
+
+      return new Species();
+    }
   }
-}
